cb443a54f24147831adc07a76742884ef94fdb61
Maria <lady.mescheryakowa@yandex.ru>
2024-03-25 23:48:02 +0300

    +

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
-let gulp = require('gulp')
-    pug = require('gulp-pug')
+// загрузка плагинов
+let gulp = require('gulp'),
+    pug = require('gulp-pug'); // конвертация паг в хтмл
+// const cssnano = require('gulp-cssnano');
+// const changed = require('gulp-changed'); // следит за изменениями
+// const browsersync = require('browser-sync').create(); // перезагружает браузер
+// const imagemin = require('gulp-imagemin'); // уменьшает размеры изображений
+// const clean = require('gulp-clean'); // может удалять папки и файлы
 
-    
+// для конвертации файлов паг в хтмл
 gulp.task('pug', function() {
     return gulp.src('dev/*.pug')
-    .pipe(pug())
+    .pipe(pug({
+        pretty: true
+    }))
     .pipe(gulp.dest('dev'))
 });
 
@@ -14,13 +22,27 @@ gulp.task('pug', function() {
 //     .pipe(gulp.dest('dev/layouts/'))
 // });
 
+// для отслеживания изменений
 gulp.task('watch', function(){
     gulp.watch('dev/layouts/*.pug', gulp.parallel('pug'));
 });
 
-
-gulp.task('watch_all', function(){
-    gulp.watch('dev/*.pug', gulp.parallel('pug'));
+gulp.task('watch_modules', function(){
+    gulp.watch('dev/modules/*.pug', gulp.parallel('pug'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'watch_all'));``
+
+// // для оптимизации (сжатия) картинок
+// function img() {
+//     return  src('./dev/src/images/*')
+//         .pipe(imagemin())
+//         .pipe(dest('./images/'));
+// }
+
+// для запуска скриптов
+gulp.task('default', gulp.parallel('watch', 'watch_modules'));
+
+
+
+// команда перезагрузки браузера
+// .pipe(browsersync.stream());
